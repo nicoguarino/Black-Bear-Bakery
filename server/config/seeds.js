4b4265318ca9c2fd94ec158446c53aba82b30582
Nico (Filipu) Guarino <nizoworld@gmail.com>
2022-01-03 14:03:45 -0800

    forgot to write out the index.js for models

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -3,72 +3,72 @@ const { User, Product, Category } = require('../models');
 
 db.once('open', async () => {
     await Category.deleteMany();
-  
+
     const categories = await Category.insertMany([
-      { name: 'Cupcakes' },
-      { name: 'Cookies' },
-      { name: 'Cakes' },
-      { name: 'Cake Truffles' },
+        { name: 'Cupcakes' },
+        { name: 'Cookies' },
+        { name: 'Cakes' },
+        { name: 'Cake Truffles' },
     ]);
-  
+
     console.log('categories seeded');
-  
+
     await Product.deleteMany();
-  
+
     const products = await Product.insertMany([
-      {
-        name: '',
-        description:
-          '',
-        image: '',
-        category: categories[0]._id,
-        price: 2.99,
-        quantity: 500
-      },
-      {
-        name: '',
-        description: '',
-        image: '',
-        category: categories[0]._id,
-        price: 1.99,
-        quantity: 500
-      },
-      {
-        name: ' ',
-        category: categories[1]._id,
-        description:
-          '',
-        image: '',
-        price: 7.99,
-        quantity: 20
-      },
+        {
+            name: '',
+            description:
+                '',
+            image: '',
+            category: categories[0]._id,
+            price: 2.99,
+            quantity: 500
+        },
+        {
+            name: '',
+            description: '',
+            image: '',
+            category: categories[0]._id,
+            price: 1.99,
+            quantity: 500
+        },
+        {
+            name: ' ',
+            category: categories[1]._id,
+            description:
+                '',
+            image: '',
+            price: 7.99,
+            quantity: 20
+        },
 
     ]);
-  
+
     console.log('products seeded');
-  
+
     await User.deleteMany();
-  
+
     await User.create({
-      firstName: 'Test',
-      lastName: 'Testing',
-      email: 'test@gmail.com',
-      password: 'Test1234',
-      orders: [
-        {
-          products: [products[0]._id, products[0]._id, products[1]._id]
-        }
-      ]
+        firstName: 'Test',
+        lastName: 'Testing',
+        email: 'test@gmail.com',
+        password: 'Test1234',
+        orders: [
+            {
+                products: [products[0]._id, products[0]._id, products[1]._id]
+            }
+        ]
     });
-  
+
     await User.create({
-      firstName: 'Group',
-      lastName: 'Testing',
-      email: 'group@gmail.com',
-      password: 'group1234'
+        firstName: 'Group',
+        lastName: 'Testing',
+        email: 'group@gmail.com',
+        password: 'group1234'
     });
-  
+
     console.log('users seeded');
-  
+
     process.exit();
-  });
+});
--- server/models/index.js
+++ server/models/index.js
@@ -4,3 +4,9 @@ const Category = require('./Category');
 const Order = require('./Order');
 
 module.exports = { User, Product, Category, Order };
+const User = require('./User');
+const Product = require('./Product');
+const Category = require('./Category');
+const Order = require('./Order');
+
+module.exports = { User, Product, Category, Order };
