53f61a45218cae8a7543ef415d495bae2371d268
Nico (Filipu) Guarino <nizoworld@gmail.com>
2022-01-03 17:52:02 -0800

    deleted duplicated code in model index

--- server/models/index.js
+++ server/models/index.js
@@ -4,9 +4,3 @@ const Category = require('./Category');
 const Order = require('./Order');
 
 module.exports = { User, Product, Category, Order };
-const User = require('./User');
-const Product = require('./Product');
-const Category = require('./Category');
-const Order = require('./Order');
-
-module.exports = { User, Product, Category, Order };
