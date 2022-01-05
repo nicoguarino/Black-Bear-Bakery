e19ab5b10a339ebb97ff7ddecab881d660ce23e1
ForeytKL22 <krforeyt@gmail.com>
2022-01-05 10:20:22 -0700

    updated and styled nav and footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
-// import Container from 'react-bootstrop/Container';
-// import { Container } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import { BsFacebook } from 'react-icons/bs';
 import { BsInstagram } from 'react-icons/bs';
 import { FaEnvelope } from 'react-icons/fa';
+import './style.css';
 
 function Footer() {
     return (
-        
-        <div class="contact">
-            <a href="https://www.facebook.com/blackbearbakerydurango" target="_blank"><BsFacebook /></a>
-            <a href="https://www.instagram.com/_blackbearbakery_/" target="_blank"><BsInstagram /></a>
-            <a href="mailto:blackbearbakerydurango@gmail.com"><FaEnvelope /></a>
+        <Container>
+        <div className="contact">
+            <a href="https://www.facebook.com/blackbearbakerydurango" className="contact-link" target="_blank"><BsFacebook /></a>
+            <a href="https://www.instagram.com/_blackbearbakery_/" className="contact-link" target="_blank"><BsInstagram /></a>
+            <a href="mailto:blackbearbakerydurango@gmail.com" className="contact-link"><FaEnvelope /></a>
         </div>
+        </Container>
 
     );
 }
