addcda9d0ee8f68fe6d50f48bfeb7d8b02a59ad9
Jaron <jjkenyon777@gmail.com>
2022-01-04 19:29:29 -0700

    home/login/signup rendering

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,55 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup'; 
+import { StoreProvider } from './utils/GlobalState';
 
+const httpLink = createHttpLink({
+  uri: '/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
-    <h1>Hello world</h1>
+    <ApolloProvider client={client}>
+    <Router>
+      <div>
+        <StoreProvider>
+          {/* <Nav /> */}
+          <Routes>
+          <Route path="/" element={<Home />}></Route>
+            <Route path="/login" element={<Login />}></Route>
+            <Route path="/signup" element={<Signup />}></Route>
+            {/* <Route exact path="/orderHistory" element={OrderHistory} />
+            <Route exact path="/products/:id" element={Detail} /> */}
+            {/* <Route element={NoMatch} /> */}
+          </Routes>
+        </StoreProvider>
+      </div>
+    </Router>
+  </ApolloProvider>
   );
 }
 
