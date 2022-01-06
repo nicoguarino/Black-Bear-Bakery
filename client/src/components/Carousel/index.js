2dabc3bd01122798b3cc272557c231e204c22d5f
Jaron <jjkenyon777@gmail.com>
2022-01-05 19:48:01 -0700

    carousel

---
+++ client/src/components/Carousel/index.js
@@ -0,0 +1,54 @@
+import React, {useState} from "react"; 
+import Carousel from 'react-bootstrap/Carousel';
+
+function ControlledCarousel() {
+    const [index, setIndex] = useState(0);
+  
+    const handleSelect = (selectedIndex, e) => {
+      setIndex(selectedIndex);
+    };
+  
+    return (
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/Test-Logo.svg/783px-Test-Logo.svg.png"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://www.outbrain.com/techblog/wp-content/uploads/2017/05/road-sign-361513_960_720.jpg"
+            alt="Second slide"
+          />
+  
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://hackernoon.com/hn-images/0*xMaFF2hSXpf_kIfG.jpg"
+            alt="Third slide"
+          />
+  
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    );
+  }
+  
+export default ControlledCarousel;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,7 @@
 import React from "react";
+// import Signup from '../components/Carousel'; 
+import ControlledCarousel from '../components/Carousel'
+
 // import ProductList from "../components/ProductList";
 // import CategoryMenu from "../components/CategoryMenu";
 // import Cart from '../components/Cart';
@@ -6,6 +9,7 @@ import React from "react";
 const Home = () => {
   return (
     <div className="container">
+      <ControlledCarousel />
     <p>Hello world!</p>
     </div>
   );
