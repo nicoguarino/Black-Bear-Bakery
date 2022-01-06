650cc5f61f9a177b6ca8dc418abc990e920faa1d
Jaron <jjkenyon777@gmail.com>
2022-01-05 20:49:23 -0700

    Success

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,8 @@ import { setContext } from '@apollo/client/link/context';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Signup from './pages/Signup'; 
+import Success from './pages/Success';
+import NoMatch from './pages/NoMatch';
 import Navigation from './components/Navigation';
 import Footer from './components/Footer';
 import { StoreProvider } from './utils/GlobalState';
@@ -45,9 +47,10 @@ function App() {
           <Route exact path="/" component={Home} />
             <Route exact path="/login" component={Login}/>
             <Route exact path="/signup" component={Signup}/>
+            <Route exact path="/success" component={Success} />
             {/* <Route exact path="/orderHistory" component={OrderHistory} />
             <Route exact path="/products/:id" component={Detail} /> */}
-            {/* <Route component={NoMatch} /> */}
+            <Route component={NoMatch} />
           </Switch>
           <Footer/>
         </StoreProvider>
