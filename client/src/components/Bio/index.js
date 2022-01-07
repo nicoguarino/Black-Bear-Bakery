c9a18dd3adee8dcdbb5bc5cac1d373c7a4a439bd
ForeytKL22 <krforeyt@gmail.com>
2022-01-07 16:59:46 -0700

    added styling and full bio to home page

--- client/src/components/Bio/index.js
+++ client/src/components/Bio/index.js
@@ -7,6 +7,9 @@ import { Col } from 'react-bootstrap';
 function Bio() {
     return (
     <Container className="bio-container">
+        <div>
+            <div class="divider div-transparent"></div>
+        </div>
         <h2>Meet the baker</h2>
         <Row>
             <Col className="col d-flex justify-content-center">
@@ -19,8 +22,18 @@ function Bio() {
                     />
                 </Figure>
             </Col>
-            <Col className="col">My name is Emily Black and I'm an avid horse lover, mother of two, and owner of Black Bear Bakery. I service Durango, CO and surrounding areas with delectable bakery items for all occasions. Birthday parties, weddings, or just for fun, I can create any delicious bakery sweet to match your needs. </Col>
+            <Col className="col">
+                <p>My name is Emily Black and I'm an avid horse lover, mother of two, and owner of Black Bear Bakery. I service Durango, CO and surrounding areas with delectable bakery items for all occasions. Birthday parties, weddings, baby showers, or just to satisfy your sweet tooth, I can create any delicious bakery item to match your needs.</p> 
+                <p>
+                When I'm not baking, you can find me riding my mustang Ella though the mountains of southwest colorado or spending time with my family - my wonderfully supportive husband, Colton, and two beautiful children, Kyler and Brinley.
+                </p>
+                If you don't find exactly what you need in my online store, I do take custom orders of any kind, so call me at (970)-238-0372 or contact me <a href="/contact">here</a>. I look forward to hearing from you!
+                
+                </Col>
         </Row>
+        <div>
+            <div class="divider div-transparent"></div>
+        </div>
       </Container>
     );
 }
